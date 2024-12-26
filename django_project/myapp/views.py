606180e67d88bfb3c804cfae75081e44aaf1359e
AlexanderBermudez <data506@hotmail.com>
2024-12-26 15:34:01 -0600

    Update of breaf

--- django_project/myapp/views.py
+++ django_project/myapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'myapp/home.html')
+
+def about_me(request):
+    return render(request, 'myapp/about_me.html')
